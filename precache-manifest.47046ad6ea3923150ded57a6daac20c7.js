952b1b303ad4dfcd518b8a5a4701e3b78cdaba7a
Tom Hart <kitsonian@gmail.com>
2022-05-06 14:18:22 -0400

    Updates

---
+++ precache-manifest.47046ad6ea3923150ded57a6daac20c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b57c2f9516363803a5bf",
+    "url": "/love-testometer/static/css/main.6b1a130e.chunk.css"
+  },
+  {
+    "revision": "b57c2f9516363803a5bf",
+    "url": "/love-testometer/static/js/main.b57c2f95.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/love-testometer/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "99b946a9c5f8def6ba95",
+    "url": "/love-testometer/static/js/2.99b946a9.chunk.js"
+  },
+  {
+    "revision": "64dd95ab786b42db6c06",
+    "url": "/love-testometer/static/js/runtime~main.64dd95ab.js"
+  },
+  {
+    "revision": "ac1ec47337e56eda84818b564a5a60dd",
+    "url": "/love-testometer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c61a23cec626103c78117dcec92b481d.js"
+  "/love-testometer/precache-manifest.47046ad6ea3923150ded57a6daac20c7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/love-testometer/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
